f6c3620d98a2d433815cf4ff2c818a17184b6034
christian-cell <61358110+christian-cell@users.noreply.github.com>
2020-04-06 16:41:49 +0200

    sanp de la practica pelis J.S

---
+++ app.js
@@ -0,0 +1,176 @@
+
+ 
+ //Estas son mis 5 peliculas de la categoria accion
+ 
+ var peliculas = document.querySelector('#pelis');
+ var pr = document.createElement('li');
+ pr.textContent = 'Proyecto Rampage';
+ peliculas.appendChild(pr);
+
+ var peliculas = document.querySelector('#pelis');
+ var pr = document.createElement('li');
+ pr.textContent = 'El Declive';
+ peliculas.appendChild(pr);
+
+ var peliculas = document.querySelector('#pelis');
+ var pr = document.createElement('li');
+ pr.textContent = 'Election';
+ peliculas.appendChild(pr);
+
+ var peliculas = document.querySelector('#pelis');
+ var pr = document.createElement('li');
+ pr.textContent = 'El Final De Todo';
+ peliculas.appendChild(pr);
+
+ var peliculas = document.querySelector('#pelis');
+ var pr = document.createElement('li');
+ pr.textContent = 'Colombiana';
+ peliculas.appendChild(pr);
+
+//estas son mis peliculas de terror
+
+ var terr = document.querySelector('#terror');
+ var els = document.createElement('li');
+ els.textContent = 'En La Sombra';
+ terr.appendChild(els);
+
+ var terr = document.querySelector('#terror');
+ var els = document.createElement('li');
+ els.textContent = 'Clinical';
+ terr.appendChild(els);
+
+ var terr = document.querySelector('#terror');
+ var els = document.createElement('li');
+ els.textContent = 'Ruins';
+ terr.appendChild(els);
+
+ var terr = document.querySelector('#terror');
+ var els = document.createElement('li');
+ els.textContent = 'La Mansion';
+ terr.appendChild(els);
+
+ var terr = document.querySelector('#terror');
+ var els = document.createElement('li');
+ els.textContent = 'AnnaBelle';
+ terr.appendChild(els);
+
+//estas son mis pelis de drama.
+
+ var drama = document.querySelector('#drama');
+           var ei = document.createElement('li');
+           ei.textContent = 'El Irlandes';
+           drama.appendChild(ei);
+
+           var drama = document.querySelector('#drama');
+           var ei = document.createElement('li');
+           ei.textContent = 'Virus';
+           drama.appendChild(ei);
+
+           var drama = document.querySelector('#drama');
+           var ei = document.createElement('li');
+           ei.textContent = 'El Sumilier';
+           drama.appendChild(ei);
+
+           var drama = document.querySelector('#drama');
+           var ei = document.createElement('li');
+           ei.textContent = 'El Francotirador';
+           drama.appendChild(ei);
+
+           var drama = document.querySelector('#drama');
+           var ei = document.createElement('li');
+           ei.textContent = 'Blow';
+           drama.appendChild(ei);
+
+
+
+           //estas son mis pelis de animacion
+
+           var animacion = document.querySelector('#animacion');
+           var lm = document.createElement('li');
+           lm.textContent = 'La mascara';
+           animacion.appendChild(lm);
+
+           var animacion = document.querySelector('#animacion');
+           var lm = document.createElement('li');
+           lm.textContent = 'Ponio';
+           animacion.appendChild(lm);
+
+
+           var animacion = document.querySelector('#animacion');
+           var lm = document.createElement('li');
+           lm.textContent = 'Godzilla';
+           animacion.appendChild(lm);
+
+           var animacion = document.querySelector('#animacion');
+           var lm = document.createElement('li');
+           lm.textContent = 'Akira';
+           animacion.appendChild(lm);
+
+
+           var animacion = document.querySelector('#animacion');
+           var lm = document.createElement('li');
+           lm.textContent = 'Blame';
+           animacion.appendChild(lm);
+
+           //estas son mis pelis de psicologia
+
+           var psico = document.querySelector('#psicologia');
+           var th = document.createElement('li');
+           th.textContent = 'Tu Hijo';
+           psico.appendChild(th);
+
+           var psico = document.querySelector('#psicologia');
+           var th = document.createElement('li');
+           th.textContent = 'El Aviso';
+           psico.appendChild(th);
+
+
+           var psico = document.querySelector('#psicologia');
+           var th = document.createElement('li');
+           th.textContent = 'Fractura';
+           psico.appendChild(th);
+
+
+           var psico = document.querySelector('#psicologia');
+           var th = document.createElement('li');
+           th.textContent = 'Madre';
+           psico.appendChild(th);
+
+
+           var psico = document.querySelector('#psicologia');
+           var th = document.createElement('li');
+           th.textContent = 'Perdido';
+           psico.appendChild(th);
+
+           //estas son mis pelis romanticas
+
+
+           var romantica = document.querySelector('#romantica');
+           var g = document.createElement('li');
+           g.textContent='Ghost';
+           romantica.appendChild(g);
+
+           var romantica = document.querySelector('#romantica');
+           var g = document.createElement('li');
+           g.textContent='Siete Alamas';
+           romantica.appendChild(g);
+
+           var romantica = document.querySelector('#romantica');
+           var g = document.createElement('li');
+           g.textContent='Desnudo';
+           romantica.appendChild(g);
+
+
+           var romantica = document.querySelector('#romantica');
+           var g = document.createElement('li');
+           g.textContent='Bellow Her .M';
+           romantica.appendChild(g);
+
+           var romantica = document.querySelector('#romantica');
+           var g = document.createElement('li');
+           g.textContent='Mi Primer Beso';
+           romantica.appendChild(g);
+
+
+
+                
